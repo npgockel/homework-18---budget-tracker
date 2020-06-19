d1ec9b1fe22bc969494ebccdcb99a9bb3a81edc8
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-06-19 15:21:06 -0700

    basic functionality set

--- budget-tracker/src/App.js
+++ budget-tracker/src/App.js
@@ -2,25 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [],
+      currentTransaction: 0,
+    }
+  }
+
+  handleTyping = (event) => {
+    //console.log('we r typing ???', parseInt(event.target.value)) 
+    this.setState({currentTransaction: parseInt(event.target.value) })
+  }
+
+  handleSave = () => {
+    var oldTransactions = this.state.transactions
+    var newTransaction = oldTransactions.concat(this.state.currentTransaction)
+    this.setState({transactions: newTransaction})
+  }
+
+  render() {
+
+    console.log('this is our state!!!', this.state)
+
+    let total = 0
+    for (let i = 0; i < this.state.transactions.length; i++) {
+     total += this.state.transactions[i]
+      
+    }
+
+    return (
+      <div>
+        <h1> Add a transaction </h1>
+        <input onChange={this.handleTyping}></input>
+        <button onClick={this.handleSave}>Save Transaction</button>
+
+        <h1> Grand Total: ${total} </h1>
+        <ul>
+        {this.state.transactions.map((singleNum)=>{
+          return(
+            <li>
+              {singleNum}
+            </li>
+          )
+        })}
+        
+
+          </ul>
+        
+
+      </div>
+    );
+
+    
+  }
 }
 
 export default App;
