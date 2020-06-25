e242ff92d69e09c05af16190de20bd8f8ffc7cdd
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-06-24 22:21:18 -0700

    Updates

---
+++ precache-manifest.1bf7b48ae4362427eb454f21c30098da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6db4e76a0552448f6c6e333bd34284",
+    "url": "/homework-18---budget-tracker/index.html"
+  },
+  {
+    "revision": "3fc315bab6af3931e3da",
+    "url": "/homework-18---budget-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab1ec6854da47a648fde",
+    "url": "/homework-18---budget-tracker/static/js/2.db708239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework-18---budget-tracker/static/js/2.db708239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc315bab6af3931e3da",
+    "url": "/homework-18---budget-tracker/static/js/main.28ce0b47.chunk.js"
+  },
+  {
+    "revision": "e3d89d2bd12b0e4733b1",
+    "url": "/homework-18---budget-tracker/static/js/runtime-main.ad9d3c5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homework-18---budget-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
